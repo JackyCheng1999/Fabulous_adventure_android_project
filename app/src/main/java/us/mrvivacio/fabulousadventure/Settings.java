3baeee32644027d0e80facd7922ab423f41012ef
Yichen Zhou <yichen11@illinois.edu>
2019-05-04 13:03:36 -0500

    user can set up avatar

--- app/src/main/java/us/mrvivacio/fabulousadventure/Settings.java
+++ app/src/main/java/us/mrvivacio/fabulousadventure/Settings.java
@@ -1,6 +1,8 @@
 package us.mrvivacio.fabulousadventure;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +11,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mylibrary.Word;
@@ -17,6 +21,11 @@ public class Settings extends AppCompatActivity {
 
     Button b1, b2, b3;
 
+    private static final int PICK_IMAGE = 100;
+
+    Uri imageUri;
+
+    ImageView avatar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +36,21 @@ public class Settings extends AppCompatActivity {
         b2 = (Button) findViewById(R.id.login2);
 
         b3 = (Button) findViewById(R.id.logout);
+
+        TextView identity = (TextView) findViewById(R.id.textView5);
+
+        avatar = (ImageView)  findViewById(R.id.avatar);
+
+        identity.setText(Word.username);
+
+        avatar.setImageResource(R.drawable.sampleavatar);
+
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,4 +90,17 @@ public class Settings extends AppCompatActivity {
     }
 
 
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            avatar.setImageURI(imageUri);
+        }
+    }
 }
